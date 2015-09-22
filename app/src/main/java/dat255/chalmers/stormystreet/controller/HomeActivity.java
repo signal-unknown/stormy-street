cf5d48344e84e2a778bd886e780b66e4a5e972a0
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-09-22 22:49:49 +0200

    Replaces legacy ActionBar with new Toolbar for more flexibility

--- app/src/main/java/dat255/chalmers/stormystreet/controller/HomeActivity.java
+++ app/src/main/java/dat255/chalmers/stormystreet/controller/HomeActivity.java
@@ -2,6 +2,7 @@ package dat255.chalmers.stormystreet.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,10 +10,14 @@ import dat255.chalmers.stormystreet.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
     }
 
 
