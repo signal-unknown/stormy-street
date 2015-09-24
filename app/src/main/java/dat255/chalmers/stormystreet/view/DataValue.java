1a484bab36216b39bcf92e150a32a742efa26b93
davve94 <fodavid@student.chalmers.se>
2015-09-24 22:24:00 +0200

    DataValue class with methods

---
+++ app/src/main/java/dat255/chalmers/stormystreet/view/DataValue.java
@@ -0,0 +1,32 @@
+package dat255.chalmers.stormystreet.view;
+
+/**
+ * Created by DavidF on 2015-09-24.
+ */
+public class DataValue {
+
+    private long id;
+    private String dataValue;
+
+
+    public long getId() {
+        return id;
+    }
+    private void setId(long id) {
+
+        this.id = id;
+    }
+    private void setDataValue(String dataValue) {
+
+        this.dataValue = dataValue;
+
+    }
+    private String getDataValue() {
+
+        return dataValue;
+    }
+
+
+}
+
+
