78b9586bd8b4a173ad86f5382087273d880ea9d8
Criticalstone <kevinhoog@gmail.com>
2015-09-24 15:16:24 +0200

    Adds Mainmodel

---
+++ app/src/main/java/dat255/chalmers/stormystreet/model/MainModel.java
@@ -0,0 +1,16 @@
+package dat255.chalmers.stormystreet.model;
+
+import java.util.List;
+
+/**
+ * @author Kevin Hoogendijk
+ * @since 2015-09-24
+ * The toplevel model of the app
+ */
+public class MainModel {
+    BusManager busManager;
+
+    public MainModel(){
+        this.busManager = new BusManager();
+    }
+}
