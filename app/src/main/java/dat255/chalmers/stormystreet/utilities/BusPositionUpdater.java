7c9762dfc0ba153d31b51dad25e8ed006b4c2bef
Alexander Karlsson <alex94@comhem.se>
2015-09-25 10:43:39 +0200

    Added bus position updater

---
+++ app/src/main/java/dat255/chalmers/stormystreet/utilities/BusPositionUpdater.java
@@ -0,0 +1,25 @@
+package dat255.chalmers.stormystreet.utilities;
+
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by alexanderkarlsson on 15-09-25.
+ */
+public class BusPositionUpdater extends AsyncTask<Void,Void,Map<LatLng, String>>{
+
+    @Override
+    protected Map<LatLng, String> doInBackground(Void... params) {
+        //TODO fetch positions, and map them to an identifier (probably registration nr)
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Map<LatLng, String> map){
+        //TODO handle positions
+    }
+}
