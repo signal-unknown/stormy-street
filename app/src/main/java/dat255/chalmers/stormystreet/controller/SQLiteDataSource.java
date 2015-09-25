e1e629ac769a9d6eed1aa093fd88c3dd867bf6b5
davve94 <fodavid@student.chalmers.se>
2015-09-25 11:10:24 +0200

    added methods: getAllDataValues, saveData, parseDataValue

--- app/src/main/java/dat255/chalmers/stormystreet/controller/SQLiteDataSource.java
+++ app/src/main/java/dat255/chalmers/stormystreet/controller/SQLiteDataSource.java
@@ -1,10 +1,14 @@
 package dat255.chalmers.stormystreet.controller;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.ContentValues;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dat255.chalmers.stormystreet.model.AppSQLiteWrapper;
 import dat255.chalmers.stormystreet.model.DataValue;
@@ -16,6 +20,7 @@ public class SQLiteDataSource {
 
     private AppSQLiteWrapper appSQLiteWrapper;
     private SQLiteDatabase database;
+    private String [] data_Table_Columns = {appSQLiteWrapper.column_id, appSQLiteWrapper.database_Name};
 
     public SQLiteDataSource(Context context) {
         appSQLiteWrapper = new AppSQLiteWrapper(context);
@@ -29,27 +34,52 @@ public class SQLiteDataSource {
         appSQLiteWrapper.close();
 
     }
-    private void saveData(String dataValue) {
+    private DataValue saveData(String dataValue) {
 
         ContentValues values  = new ContentValues();
         values.put(appSQLiteWrapper.column_Data, dataValue);
 
         long addId = database.insert(appSQLiteWrapper.table, null, values);
-       // Cursor cursor = database.query(appSQLiteWrapper.table, null, )
+        Cursor cursor = database.query(appSQLiteWrapper.table, data_Table_Columns, appSQLiteWrapper.column_id + " = " + addId,
+                null, null, null, null);
+        cursor.moveToFirst();
 
+        DataValue newDataValue = parseDatavalue(cursor);
+        cursor.close();
 
+        return newDataValue;
+    }
+    private DataValue parseDatavalue(Cursor cursor) {
 
-
-
+        DataValue dataValue = new DataValue();
+        dataValue.setId(cursor.getInt(0));
+        dataValue.setName(cursor.getString(1));
+        return dataValue;
     }
     private void deleteData(DataValue dataValue) {
 
         long id = dataValue.getId();
-        Log.i("Delete", "Bus data deleted with: " + id );
-        database.delete(appSQLiteWrapper.table,appSQLiteWrapper.column_id + " = " + id, null);
+        Log.i("Delete", "Bus data deleted with: " + id);
+        database.delete(appSQLiteWrapper.table, appSQLiteWrapper.column_id + " = " + id, null);
     }
+    private List getAllDataValues() {
+        List dataList = new ArrayList();
 
+        Cursor cursor = database.query(appSQLiteWrapper.table,data_Table_Columns,null,null,null,null,null);
+        cursor.moveToFirst();
 
+        while(!cursor.isAfterLast()) {
+
+        DataValue dataValue = parseDatavalue(cursor);
+        dataList.add(dataValue);
+        cursor.moveToNext();
+
+        }
+
+        cursor.close();
+
+        return dataList;
+    }
 }
 
 
