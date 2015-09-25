dd80356459d59724cf562455b8360c6c82d05e6c
Maxim Goretskyy <13maxim13@live.se>
2015-09-25 11:08:26 +0200

    Adds debug information

--- app/src/main/java/dat255/chalmers/stormystreet/services/WifiService.java
+++ app/src/main/java/dat255/chalmers/stormystreet/services/WifiService.java
@@ -7,12 +7,12 @@ import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import dat255.chalmers.stormystreet.Constants;
-import dat255.chalmers.stormystreet.GlobalState;
 
 /**
  * @author Maxim Goretskyy
@@ -22,7 +22,9 @@ public class WifiService extends IntentService {
     private WifiManager mWifiManager;
     private List<String> macAddresses;
     private final int DELAY_TIME = 30000; //30 seconds
-
+    public WifiService(){
+        super("dat255.chalmers.stormystreet.services.WifiService");
+    }
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
      *
@@ -34,16 +36,20 @@ public class WifiService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d("Wifiservice", "Received intent: " + intent.getAction());
         if(intent.getAction().equals(Constants.ACTION_GET_MAC)){
             scanMacs();
+            Log.d("Wifiservice", "Scanned MACs");
 
             if(isNearBus()){
+                Log.d("Wifiservice", "Near a bus");
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         scanMacs();//if you still can see the mac address of the bus after 30 seconds
                                     //then you are probably on the bus
                         if(isNearBus()){
+                            Log.d("Wifiservice", "On bus");
                             //Todo update model with mac addresses and if user is near bus
                         }
                     }
@@ -68,8 +74,12 @@ public class WifiService extends IntentService {
         macAddresses.clear();
         for(ScanResult network : mScanResults){
             macAddresses.add(network.BSSID.toLowerCase());
+            Log.d("Scanned",network.BSSID.toLowerCase());
         }
+
     }
+
+
 
     public List<String> getMacAddresses(){
         return this.macAddresses;
