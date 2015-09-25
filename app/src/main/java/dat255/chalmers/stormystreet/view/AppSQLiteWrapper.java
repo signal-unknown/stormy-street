869e02a3281eecae9872907f349bbc8c6a29a3e7
davve94 <fodavid@student.chalmers.se>
2015-09-25 09:32:47 +0200

    fixed bugs

--- app/src/main/java/dat255/chalmers/stormystreet/view/AppSQLiteWrapper.java
+++ app/src/main/java/dat255/chalmers/stormystreet/view/AppSQLiteWrapper.java
@@ -8,16 +8,17 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
-import android.util.Log;
 
 
 public class AppSQLiteWrapper extends SQLiteOpenHelper {
 
-    private static final String columns = "value";
-    private static final String table = "Values";
-    private static final String database_Name = "ValueStorage.db";
+    public static final String column_id = "value";
+    public static final String table = "Values";
+    public static final String column_Data = "data";
+
+    public static final String database_Name = "ValueStorage.db";
     private static final int version = 1;
-    private static final String create_Database = "Create table " + table + " "  + columns;
+    private static final String create_Database = "Create table " + table + " "  + column_id;
 
 
     public AppSQLiteWrapper(Context context) {
@@ -28,7 +29,6 @@ public class AppSQLiteWrapper extends SQLiteOpenHelper {
     @Override
     public void onCreate (SQLiteDatabase database) {
 
-        //create database
 
         database.execSQL(create_Database);
 
@@ -36,7 +36,6 @@ public class AppSQLiteWrapper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-        Log.i(AppSQLiteWrapper.class.getName(), "upgrading database to " + newVersion + " from " + oldVersion);
         db.execSQL(table);
         onCreate(db);
 
