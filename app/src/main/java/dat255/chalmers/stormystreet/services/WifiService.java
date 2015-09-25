edb9f8f057d520eff146c1c0cbafe409f1a152eb
Maxim Goretskyy <13maxim13@live.se>
2015-09-25 16:23:00 +0200

    Adds 30 sec check to determine if you are on bus

--- app/src/main/java/dat255/chalmers/stormystreet/services/WifiService.java
+++ app/src/main/java/dat255/chalmers/stormystreet/services/WifiService.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -21,6 +23,7 @@ import dat255.chalmers.stormystreet.Constants;
 public class WifiService extends IntentService {
     private WifiManager mWifiManager;
     private List<String> macAddresses;
+    private HandlerThread hThread;
     private final int DELAY_TIME = 30000; //30 seconds
     public WifiService(){
         super("dat255.chalmers.stormystreet.services.WifiService");
@@ -43,17 +46,24 @@ public class WifiService extends IntentService {
 
             if(isNearBus()){
                 Log.d("Wifiservice", "Near a bus");
-                new Handler().postDelayed(new Runnable() {
+                /*Todo use API to check if you are connected to network as well
+                    might not need to wait 30 secs then
+                */
+                hThread = new HandlerThread("WifiService");
+                hThread.start();
+                Handler mHandler = new Handler(hThread.getLooper());
+                Runnable mRun = new Runnable() {
                     @Override
-                    public void run() {
-                        scanMacs();//if you still can see the mac address of the bus after 30 seconds
-                                    //then you are probably on the bus
+                    public synchronized void run() {
+                        Log.d("Wifiservice", "Inside new thread");
+                        scanMacs();
                         if(isNearBus()){
                             Log.d("Wifiservice", "On bus");
                             //Todo update model with mac addresses and if user is near bus
                         }
                     }
-                }, DELAY_TIME);
+                };
+                mHandler.postDelayed(mRun, DELAY_TIME);
             }
         }
     }
@@ -74,7 +84,7 @@ public class WifiService extends IntentService {
         macAddresses.clear();
         for(ScanResult network : mScanResults){
             macAddresses.add(network.BSSID.toLowerCase());
-            Log.d("Scanned",network.BSSID.toLowerCase());
+            Log.d("WifiService: Scanned",network.BSSID.toLowerCase());
         }
 
     }
