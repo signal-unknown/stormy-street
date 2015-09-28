b2d91b27020fc545d97ea5d17709d33ab465809e
Alexander Karlsson <alex94@comhem.se>
2015-09-28 11:31:02 +0200

    Added URL building

--- app/src/main/java/dat255/chalmers/stormystreet/utilities/BusPositionUpdater.java
+++ app/src/main/java/dat255/chalmers/stormystreet/utilities/BusPositionUpdater.java
@@ -4,16 +4,49 @@ import android.os.AsyncTask;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
 /**
- * Created by alexanderkarlsson on 15-09-25.
+ * Task for fetching and parsing GPS data from ElectriCitys API
  */
 public class BusPositionUpdater extends AsyncTask<Void,Void,Map<LatLng, String>>{
 
     @Override
     protected Map<LatLng, String> doInBackground(Void... params) {
         //TODO fetch positions, and map them to an identifier (probably registration nr)
+
+        //Create proper URLs
+
+        //Base URLs and parameters
+        String uRLLatitude = "https://ece01.ericsson.net:4443/ecity";
+        String uRLLongitude = uRLLatitude + "Ericsson$Longitude2_Value";
+        uRLLatitude += "?resourceSpec=Ericsson$Latitude2_Value";
+
+        //Get current time and a timestamp 15 seconds before that
+        long curTime = System.currentTimeMillis();
+        long oldTime = curTime - 15000;
+
+        //Add times to base URLs
+        uRLLongitude += "&t1=" + oldTime + "&t2=" + curTime;
+        uRLLatitude += "&t1=" + oldTime + "&t2=" + curTime;
+
+        try {
+            URL urlLat = new URL(uRLLatitude);
+            HttpURLConnection httpLat = (HttpURLConnection) urlLat.openConnection();
+            httpLat.setRequestMethod("GET");
+        } catch (MalformedURLException e) {
+            //TODO deal with it
+            e.printStackTrace();
+        } catch (IOException e) {
+            //TODO deal with it
+            e.printStackTrace();
+        }
         return null;
     }
 
