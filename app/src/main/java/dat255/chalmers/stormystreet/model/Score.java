0d79675f4862e371dc026b2671bf4ef46b3bc1dc
Criticalstone <kevinhoog@gmail.com>
2015-09-28 11:45:13 +0200

    Adds score class

---
+++ app/src/main/java/dat255/chalmers/stormystreet/model/Score.java
@@ -0,0 +1,23 @@
+package dat255.chalmers.stormystreet.model;
+
+/**
+ * @author Kevin Hoogendijk
+ * @since 2015-09-28
+ */
+public class Score implements IScore {
+    private long value;
+    private String suffix;
+
+    public Score(long value, String suffix){
+        this.value = value;
+        this.suffix = suffix;
+    }
+
+    public long getValue() {
+        return this.value;
+    }
+
+    public String getSuffix() {
+        return this.suffix;
+    }
+}
