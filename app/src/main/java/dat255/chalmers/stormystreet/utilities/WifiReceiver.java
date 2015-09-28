e68c1b920ea13153504ac3b7eed903f33a9916f8
Maxim Goretskyy <13maxim13@live.se>
2015-09-28 14:09:12 +0200

    Adds intent in receiver for wifi state

--- app/src/main/java/dat255/chalmers/stormystreet/utilities/WifiReceiver.java
+++ app/src/main/java/dat255/chalmers/stormystreet/utilities/WifiReceiver.java
@@ -21,10 +21,16 @@ public class WifiReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d("Wifireceiver", "Received intent: " + intent.getAction());
         Log.d("Wifireceiver", "Intent we compare with: " + WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+
         if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
             Log.d("Wifireceiver", "Inside if");
             wifiIntent = new Intent(context, WifiService.class);
             wifiIntent.setAction(Constants.ACTION_GET_MAC);
+            context.startService(wifiIntent);
+        }else if (intent.getAction().equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION)){
+            Log.d("Wifireceiver", "Supp conn change action");
+            wifiIntent = new Intent(context, WifiService.class);
+            wifiIntent.setAction(Constants.ACTION_WIFI_CHANGED);
             context.startService(wifiIntent);
         }
     }
