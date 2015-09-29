71807dda07d5f6f9cd19b53dacd62b1868bed1cc
CrashIT14 <al3xand3r.h@gmail.com>
2015-09-29 23:13:22 +0200

    Refines the parallax image in profile screen for better text visibillity

--- app/src/main/java/dat255/chalmers/stormystreet/controller/ProfileActivity.java
+++ app/src/main/java/dat255/chalmers/stormystreet/controller/ProfileActivity.java
@@ -1,9 +1,13 @@
 package dat255.chalmers.stormystreet.controller;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import dat255.chalmers.stormystreet.R;
@@ -11,13 +15,35 @@ import dat255.chalmers.stormystreet.R;
 public class ProfileActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private ImageView profileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        profileImageView = (ImageView) findViewById(R.id.profileImage);
+
+        setupToolbar();
+
+        Bitmap profileImage = BitmapFactory.decodeResource(getResources(),
+                R.drawable.drawer_header);
+
+        profileImageView.setImageBitmap(profileImage);
+
+        collapsingToolbarLayout.setTitle("John Doe");
+
+        TextView text = (TextView) findViewById(R.id.debug_text);
+        for (int i = 0; i < 100; i++) {
+            text.append("Hello I am a test. LOL \n");
+        }
+    }
+
+    private void setupToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -27,11 +53,6 @@ public class ProfileActivity extends AppCompatActivity {
                 }
             });
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-
-        TextView text = (TextView) findViewById(R.id.debug_text);
-        for (int i = 0; i < 100; i++) {
-            text.append("Hello I am a test. LOL \n");
         }
     }
 }
