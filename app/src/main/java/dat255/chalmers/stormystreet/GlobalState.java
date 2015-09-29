21c5a948560be4453b1675b0e97956328fef6163
Alexander Karlsson <alex94@comhem.se>
2015-09-29 10:41:06 +0200

    Removed unnecessary API fields

--- app/src/main/java/dat255/chalmers/stormystreet/GlobalState.java
+++ app/src/main/java/dat255/chalmers/stormystreet/GlobalState.java
@@ -6,22 +6,9 @@ import android.util.Log;
 
 public class GlobalState extends Application {
 
-    private static String ELECTRICITY_KEY;
-    private static String MAPS_KEY;
-
     @Override
     public void onCreate(){
         super.onCreate();
         Log.i("Globalstate", "App started");
-        ELECTRICITY_KEY = getString(R.string.electricity_api);
-        MAPS_KEY = getString(R.string.google_maps_key);
-    }
-
-    public static String getElectricityKey(){
-        return ELECTRICITY_KEY;
-    }
-
-    public static String getMapsKey(){
-        return MAPS_KEY;
     }
 }
