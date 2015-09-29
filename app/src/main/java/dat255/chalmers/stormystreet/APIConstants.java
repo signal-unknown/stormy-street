d1b614df0cd46e5c367affef5ac842aa0c5c05cb
Alexander Karlsson <alex94@comhem.se>
2015-09-29 10:39:59 +0200

    Added API Keys file

---
+++ app/src/main/java/dat255/chalmers/stormystreet/APIConstants.java
@@ -0,0 +1,8 @@
+package dat255.chalmers.stormystreet;
+
+/**
+ * Contains API Keys
+ */
+public class APIConstants {
+    public static final String ELECTRICITY_API_KEY = "ENTER ELECTRICITY API";
+}
