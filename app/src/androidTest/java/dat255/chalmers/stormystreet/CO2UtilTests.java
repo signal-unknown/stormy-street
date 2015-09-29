90b1d72ca6ae24f60fe49f8c4ebe24dd8c7925a8
CrashIT14 <al3xand3r.h@gmail.com>
2015-09-30 00:22:44 +0200

    Implements a util class for getting CO2 savings based on kilometers

---
+++ app/src/androidTest/java/dat255/chalmers/stormystreet/CO2UtilTests.java
@@ -0,0 +1,31 @@
+package dat255.chalmers.stormystreet;
+
+import android.test.ActivityTestCase;
+
+import dat255.chalmers.stormystreet.util.CO2Util;
+
+/**
+ * @author Alexander Håkansson
+ */
+public class CO2UtilTests extends ActivityTestCase {
+
+    public void testDefaultSettingIsALL() {
+        long km = 100;
+        long expected = km * 151;
+        assertEquals("Util should default to region ALL", expected, CO2Util.getGramsSavedPerKm(km));
+    }
+
+    public void testRegionIsSaved() {
+        CO2Util.setRegionToUse(CO2Util.CO2EmissionRegion.EU);
+        long km = 100;
+        long expected = km * 130;
+        assertEquals("Region should be saved to EU", expected, CO2Util.getGramsSavedPerKm(km));
+    }
+
+    public void testActuallyUsingDefinedRegion() {
+        CO2Util.setRegionToUse(CO2Util.CO2EmissionRegion.ALL);
+        long km = 100;
+        long expected = km * 130;
+        assertEquals("The provided region should be used over the default one", expected, CO2Util.getGramsSavedPerKm(km, CO2Util.CO2EmissionRegion.EU));
+    }
+}
