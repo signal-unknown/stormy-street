98acfa5b46974a7a571a31ce01edf9de587eebdb
CrashIT14 <al3xand3r.h@gmail.com>
2015-09-29 14:10:44 +0200

    Adds back stack with back button to profile activity

--- app/src/main/java/dat255/chalmers/stormystreet/controller/ProfileActivity.java
+++ app/src/main/java/dat255/chalmers/stormystreet/controller/ProfileActivity.java
@@ -1,10 +1,9 @@
 package dat255.chalmers.stormystreet.controller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import dat255.chalmers.stormystreet.R;
@@ -20,32 +19,19 @@ public class ProfileActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         TextView text = (TextView) findViewById(R.id.debug_text);
         for (int i = 0; i < 100; i++) {
             text.append("Hello I am a test. LOL \n");
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
