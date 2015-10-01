9be1c2398b05f4ded1677c1e9b51b5329f0bf699
David Fogelberg <fodavid@student.chalmers.se>
2015-10-01 20:14:47 +0200

    Added new BusInfoActivity class

---
+++ app/src/main/java/dat255/chalmers/stormystreet/controller/BusInfoActivity.java
@@ -0,0 +1,22 @@
+package dat255.chalmers.stormystreet.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import dat255.chalmers.stormystreet.R;
+
+/**
+ * Created by DavidF on 2015-10-01.
+ */
+public class BusInfoActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_info);
+    }
+
+
+
+}
