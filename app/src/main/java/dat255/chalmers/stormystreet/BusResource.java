f017f49bfff3cb721f00a9f9456ae3a32d5f4f26
Maxim Goretskyy <13maxim13@live.se>
2015-10-02 16:34:48 +0200

    Adds enum for bus resources in api

---
+++ app/src/main/java/dat255/chalmers/stormystreet/BusResource.java
@@ -0,0 +1,10 @@
+package dat255.chalmers.stormystreet;
+
+/**
+ * @author Maxim Goretskyy
+ * Created by maxim on 15-10-02.
+ * Enum representing the resources that we use in our application
+ */
+public enum BusResource {
+    Total_Vehicle_Distance_Value
+}
