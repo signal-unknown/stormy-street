fb891faab8cf62b98bcf8beb210a64350ce82e1a
Alexander Håkansson <al3xand3r.h@gmail.com>
2015-10-06 01:40:39 +0200

    Refactors the bus info activity

--- app/src/main/java/dat255/chalmers/stormystreet/controller/BusInfoActivity.java
+++ app/src/main/java/dat255/chalmers/stormystreet/controller/BusInfoActivity.java
@@ -9,28 +9,56 @@ import android.view.View;
 import android.widget.TextView;
 import java.lang.Thread;
 
+import dat255.chalmers.stormystreet.Constants;
+import dat255.chalmers.stormystreet.GlobalState;
 import dat255.chalmers.stormystreet.R;
 import dat255.chalmers.stormystreet.model.*;
+import dat255.chalmers.stormystreet.model.bus.BusNotFoundException;
+import dat255.chalmers.stormystreet.model.bus.IBus;
 
-/**
- * Created by DavidF on 2015-10-01.
- */
+// TODO: Use model listener to automatically get when there is new data - no need for updating thread
 public class BusInfoActivity extends AppCompatActivity {
 
-    public TextView busInfo;
+    private static final int UPDATE_INTERVAL = 3000; // How often the bus info will update
+
+    public TextView busInfoText;
     private Toolbar toolbar;
-    private MainModel model = new MainModel();
     GpsCoord gpsInfo;
     public Boolean stopThread = false;
 
+
+    private IBus busInfo;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_info);
 
+        setupToolbar();
+        getBusData();
+
+        busInfoText = (TextView) findViewById(R.id.BusInfoView);
+        new Thread(new UpdateInfo()).start(); // starting new thread
+
+    }
+
+    private void getBusData() {
+        String vinNumber = getIntent().getStringExtra(Constants.EXTRA_BUS_INFO_BUS_ID);
+        try {
+            if (vinNumber != null) {
+                busInfo = ((GlobalState)getApplication()).getModel().getBus(Integer.parseInt(vinNumber));
+            } else {
+                throw new BusNotFoundException();
+            }
+        } catch (BusNotFoundException e) {
+            finish(); // Close the activity
+        }
+    }
+
+    private void setupToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
+
         if (getSupportActionBar() != null) {
             toolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
@@ -41,29 +69,20 @@ public class BusInfoActivity extends AppCompatActivity {
             });
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        busInfo = (TextView) findViewById(R.id.BusInfoView);
-        new Thread(new UpdateInfo()).start();      // starting new thread
-
     }
-    public void getBusInfo(int v) {             // Getting info from model
-        busInfo.setText("Information about selected bus: " + "\n \n" +
-                "User name: "
-                + model.getCurrentUsername() + "\n\n" +
-                "Statistics:  "
-                + model.getCurrentUser().getStatistics() + "\n\n " +
-               "Distance/time:  "
-                + model.getCurrentTrip() + "\n" + "\n\n" +
-                "Updating : " + v);
 
-        //Add other information like GPS here
+    // Getting info from model
+    private void getBusInfo(int v) {
+        // TODO: Show info from model
     }
-    class UpdateInfo implements Runnable {
+
+    private class UpdateInfo implements Runnable {
         int v = 0;
         @Override
         public void run() {
-            while (stopThread != true) {
+            while (!stopThread) {
                 try {
-                    Thread.sleep(3000);  // calling thread every 3000 ms = 3 seconds for updating information about selected bus...
+                    Thread.sleep(UPDATE_INTERVAL);
                     runOnUiThread(new Runnable() {   // for modifying view
                         @Override
                         public void run() {
@@ -74,7 +93,6 @@ public class BusInfoActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Log.d("Bus info: ", "................This thread is doing some work....................." );
             }
         }
     }
