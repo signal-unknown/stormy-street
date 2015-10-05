09b94f43f2132bd1f57b1c4db9078580cb3e698d
David Fogelberg <fodavid@student.chalmers.se>
2015-10-05 22:38:49 +0200

    refactor of BusInfoActivity

--- app/src/main/java/dat255/chalmers/stormystreet/controller/BusInfoActivity.java
+++ app/src/main/java/dat255/chalmers/stormystreet/controller/BusInfoActivity.java
@@ -1,16 +1,13 @@
 package dat255.chalmers.stormystreet.controller;
 
-import android.app.Activity;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import java.lang.Thread;
-
-import java.util.Random;
 
 import dat255.chalmers.stormystreet.R;
 import dat255.chalmers.stormystreet.model.*;
@@ -20,11 +17,11 @@ import dat255.chalmers.stormystreet.model.*;
  */
 public class BusInfoActivity extends AppCompatActivity {
 
-     public TextView info;
+    public TextView busInfo;
     private Toolbar toolbar;
     private MainModel model = new MainModel();
+    GpsCoord gpsInfo;
     public Boolean stopThread = false;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,114 +29,53 @@ public class BusInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_info);
 
-
-
-
         toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             toolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    stopThread = true;
+                    stopThread = true; // stop thread
                     onBackPressed();
-
                 }
             });
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-          //  Thread thread = Thread.currentThread();
-            //thread.interrupt();
-
-
         }
-
-
-        info = (TextView) findViewById(R.id.BusInfoView);
-
-        info.setText("lol");
-
-        new Thread(new UpdateInfo()).start();
-
+        busInfo = (TextView) findViewById(R.id.BusInfoView);
+        new Thread(new UpdateInfo()).start();      // starting new thread
 
     }
-    public void getInfo() {      // Test method ############################
+    public void getBusInfo(int v) {             // Getting info from model
+        busInfo.setText("Information about selected bus: " + "\n \n" +
+                "User name: "
+                + model.getCurrentUsername() + "\n\n" +
+                "Statistics:  "
+                + model.getCurrentUser().getStatistics() + "\n\n " +
+               "Distance/time:  "
+                + model.getCurrentTrip() + "\n" + "\n\n" +
+                "Updating : " + v);
 
-        Random rnd = new Random();
-
-        int randomID = rnd.nextInt(4);
-
-        switch(randomID) {
-
-            case 0:
-                info.setText("BUS INFO HERE!");
-                break;
-
-            case 1:
-                info.setText("Plattformen bygger på mjukvara från Ericsson och kan skalas upp utanför demoarenan." +
-                        " Det innebär att nya datakällor inom eller utom ElectriCity kan läggas till kontinuerligt under 2015-2018 och även efter det, om plattformen fortlever. " +
-                        "Utvecklingen av ElectriCitys innovationsplattform kan därmed lägga grunden till en regional innovationsplattform som stärker innovation" +
-                        " och samverkan i regionen i allmänhet och i framtida samverkansprojekt i synnerhet. ");
-                break;
-
-            case 2:
-                info.setText("Framtagandet av plattformen sker i ett samverkansprojekt finansierat av Regionutvecklingsnämnden, Västra Götalandsregionen.\n" +
-                        "\n" +
-                        "Parter i projektet är Chalmers, Cybercom, Ericsson, Icomera, Interactive Swedish ICT, Keolis, Lindholmen Science Park, Pilotfish, Viktoria Swedish ICT, Volvo Bussar och Västtrafik.");
-                break;
-
-            case 3:
-                info.setText("Innovationsplattformen tillhandahåller framförallt information om de el- och hybridbussarna som trafikerar linje 55 samt om dess hållplatser, " +
-                        "men även om kollektivtrafiken i Västra Götalandsregionen i allmänhet. " +
-                        "Informationen kan bl.a användas som underlag för att skapa koncept och prototyper till Electricity Innovation Challenge 2015");
-                break;
-
-
-        }
-
-    }
-    public void getBusInfo(int v) {             // Getting info from model //////////////////////////////////////
-
-       info.setText("Information about selected bus: " + "\n \n" + "User name: " +  model.getCurrentUsername() + "\n\n" + "Statistics: " + model.getCurrentUser().getStatistics() + "\n\n " +
-               "Distance/time: " + model.getCurrentTrip()
-               + "\n\n" + "Calling thread #: " + v);
-
-
+        //Add other information like GPS here
     }
     class UpdateInfo implements Runnable {
         int v = 0;
         @Override
         public void run() {
-
             while (stopThread != true) {
-
                 try {
-
-                    Thread.sleep(3000);  // 3 seconds
+                    Thread.sleep(3000);  // calling thread every 3000 ms = 3 seconds for updating information about selected bus...
                     runOnUiThread(new Runnable() {   // for modifying view
                         @Override
                         public void run() {
-
-                            //getInfo();
                             getBusInfo(v);
                             v++;
-
-
                         }
                     });
-
                 } catch (InterruptedException e) {
-
                     e.printStackTrace();
-
                 }
-                // do stuff here
-                Log.d("Bus info: ", "....................................." );
-
+                Log.d("Bus info: ", "................This thread is doing some work....................." );
             }
         }
-
-
     }
-
 }
