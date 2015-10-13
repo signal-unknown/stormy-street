9993c7393c795a32e52bd33fb654ae41ee20b44d
Alexander Karlsson <alex94@comhem.se>
2015-10-13 19:34:44 +0200

    Added service which updates the current score to facebook

---
+++ app/src/main/java/dat255/chalmers/stormystreet/services/FacebookScoreUpdaterService.java
@@ -0,0 +1,50 @@
+package dat255.chalmers.stormystreet.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import dat255.chalmers.stormystreet.model.MainModel;
+
+/**
+ * Service that updates the users score to facebook
+ * @author Alexander Karlsson
+ */
+public class FacebookScoreUpdaterService extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        GraphRequest request = null;
+        Bundle bundle = intent.getExtras();
+        MainModel model = (MainModel) bundle.get("MainModel");
+        final long score = model.getTotalScore().getValue();
+
+        if(accessToken != null && !accessToken.isExpired()) {
+            try {
+                request = GraphRequest.newPostRequest(
+                        accessToken,
+                        "/me/scores",
+                        new JSONObject("{\"score\":\"" + score + "\"}"),
+                        new GraphRequest.Callback() {
+                            @Override
+                            public void onCompleted(GraphResponse response) {
+                                Log.d("FacebookSUS", "Updated facebook with the score " + score);
+                            }
+                        });
+            } catch (JSONException e) {
+                //Should never occur
+                e.printStackTrace();
+            }
+            request.executeAsync();
+        }
+    }
+}
