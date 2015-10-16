9b26967dd3d0854124719887101380b91d386079
Maxim Goretskyy <13maxim13@live.se>
2015-10-16 11:01:12 +0200

    Adds comments, changes order of methods

--- app/src/main/java/dat255/chalmers/stormystreet/services/WifiService.java
+++ app/src/main/java/dat255/chalmers/stormystreet/services/WifiService.java
@@ -23,6 +23,13 @@ import dat255.chalmers.stormystreet.model.bus.BusTrip;
 /**
  * @author Maxim Goretskyy
  * Created by maxim on 15-09-24.
+ *
+ * This service takes care of when a user is stepping on and off the bus.
+ * When your phone scans for mac-addresses and notices one of the busses, it waits a DELAY_TIME
+ * (30 seconds) before scanning again and checking if you are still near a bus. We conclude that you
+ * are on the bus and start the timers, and tell CurrentTripService to start.
+ * Once you turn off your Wifi or walk away from the bus range, the service will stop timer and tell
+ * CurrentTripService to stop.
  */
 public class WifiService extends IntentService {
     private WifiManager mWifiManager;
@@ -46,6 +53,13 @@ public class WifiService extends IntentService {
         super(name);
     }
 
+
+    /*
+        Receives intent from WifiReceiver.
+        If you are near a bus (by MAC-address range) then start counting, otherwise stop counting
+        incase you moved away from a bus and finished your trip.
+        If your wifi state changed, check if you finished your trip as well. AKA Wifi turned off.
+     */
     @Override
     protected synchronized void onHandleIntent(Intent intent) {
         Log.d("Wifiservice", "Received intent: " + intent.getAction());
@@ -69,7 +83,9 @@ public class WifiService extends IntentService {
             checkEndPoint();
         }
     }
-
+    /*
+        Checks if you are near a bus.
+     */
     public synchronized boolean isNearBus(){
         for(String mac : Constants.busMacVin.values()){
             if(macAddresses.contains(mac)){
@@ -81,15 +97,10 @@ public class WifiService extends IntentService {
         return false;
     }
 
-    public synchronized void setCurrBus(String mac){
-        for(Map.Entry<Integer, String> entry : Constants.busMacVin.entrySet()){
-            if(entry.getValue().equals(mac)){
-                this.currBusNum = entry.getKey();
-                Log.d("Wifiservice", "Curr buss num: " + this.currBusNum);
-            }
-        }
-    }
 
+    /*
+        Scan BSSIDs/MACs around you.
+     */
     public synchronized void scanMacs(){
         mWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         List<ScanResult> mScanResults = mWifiManager.getScanResults();
@@ -101,27 +112,12 @@ public class WifiService extends IntentService {
         }
 
     }
-    public synchronized void resetTimestamps(){
-        startTime = 0;
-        endTime =0;
-    }
-    public synchronized void checkEndPoint(){
-        Log.d("Wifiservice", "Inside checkendpoint");
-        if(startTime !=0 && endTime == 0 ){//double check
-            Log.d("Wifiservice", "No longer on bus");
-            endTime = System.currentTimeMillis();
-            Log.d("Wifiservice", "Endtime is " + endTime);
-            //update model with end time here
-            MainModel model = ((GlobalState) getApplication()).getModel();
-            long distance = model.getCurrentTrip().getDistance();
-            Log.d("Wifiservice", "Distance is " + distance);
-            model.addBusTrip(new BusTrip(startTime, endTime, distance));
-            model.setCurrentTrip(null);
-            resetTimestamps();
-            setUserOnBus(false);
-        }
-    }
+    /*
+    Starts count if you have been near the same bus after 30 seconds since the first time.
+    Same bus is determined by the uniquenees of the mac/bssid.
+    Tells model to start a new currentTrip if succeeded.
 
+    */
     public synchronized void startCount(){
         hThread = new HandlerThread("WifiService");
         hThread.start();
@@ -141,17 +137,54 @@ public class WifiService extends IntentService {
                     startTime = System.currentTimeMillis()-DELAY_TIME;//compensating for delay
                     Log.d("Wifiservice", startTime + " my starttime");
                     Log.d("WifiService", "Current bus number new " + currBusNum);
-                    model.setCurrentTrip(new CurrentTrip(startTime, currBusNum)); //Set correct distance
+                    model.setCurrentTrip(new CurrentTrip(startTime, currBusNum));
                     setUserOnBus(true);
 
-                } else {
-                    //setUserOnBus(false);
                 }
             }
         };
         mHandler.postDelayed(mRun, DELAY_TIME);
 
     }
+    /*
+        Checks if you're done with your trip.
+        If you finished the trip, it tells model to add a new finished busTrip.
+        And reset currentTrip as well as timestamps.
+
+     */
+    public synchronized void checkEndPoint(){
+        Log.d("Wifiservice", "Inside checkendpoint");
+        if(startTime !=0 && endTime == 0 ){//double check
+            Log.d("Wifiservice", "No longer on bus");
+            endTime = System.currentTimeMillis();
+            Log.d("Wifiservice", "Endtime is " + endTime);
+            //update model with end time here
+            MainModel model = ((GlobalState) getApplication()).getModel();
+            long distance = model.getCurrentTrip().getDistance();
+            Log.d("Wifiservice", "Distance is " + distance);
+            model.addBusTrip(new BusTrip(startTime, endTime, distance));
+            model.setCurrentTrip(null);
+            resetTimestamps();
+            setUserOnBus(false);
+        }
+    }
+    /*
+    Sets the bus-VIN number of the current bus you are on.
+    */
+    public synchronized void setCurrBus(String mac){
+        for(Map.Entry<Integer, String> entry : Constants.busMacVin.entrySet()){
+            if(entry.getValue().equals(mac)){
+                this.currBusNum = entry.getKey();
+                Log.d("Wifiservice", "Curr buss num: " + this.currBusNum);
+            }
+        }
+    }
+
+    /*
+        Handles logic when the CurrentTripService should be active or not.
+        If you are on bus, the CurrentTripService is active and should be turned on, otherwise it
+        should be turnedo off.
+     */
     public synchronized void setUserOnBus(boolean isOn){
         Intent currentTripIntent = new Intent(getApplicationContext(), CurrentTripService.class);
 
@@ -164,6 +197,11 @@ public class WifiService extends IntentService {
 
         }
         ((GlobalState)getApplication()).getModel().setIsOnBus(isOn);
+    }
+
+    public synchronized void resetTimestamps(){
+        startTime = 0;
+        endTime =0;
     }
 
     public List<String> getMacAddresses(){
