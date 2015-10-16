fd23bbb5e69914fe8fe038886c7401329d908512
Maxim Goretskyy <13maxim13@live.se>
2015-10-16 14:14:34 +0200

    Adds comments

--- app/src/main/java/dat255/chalmers/stormystreet/utilities/WifiReceiver.java
+++ app/src/main/java/dat255/chalmers/stormystreet/utilities/WifiReceiver.java
@@ -12,18 +12,26 @@ import dat255.chalmers.stormystreet.services.WifiService;
 /**
  * @author Maxim Goretskyy
  * Created on 15-09-23.
+ * This class receives broadcasts from operative system of android.
+ * Different actions are taking upon different intents that were sent.
+ *
  */
 public class WifiReceiver extends BroadcastReceiver {
 
     private Intent wifiIntent;
 
+    /*
+        Starts different services depending on the received intent.
+        If new mac addresses are seen, starts WifiService to check if you are near a bus, or just
+        went off a bus and finished your trip.
+
+        If you changed your connection, it assumes you turned off your wifi therefore it starts
+        service and tells it to check if you have finished your trip.
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("Wifireceiver", "Received intent: " + intent.getAction());
-        Log.d("Wifireceiver", "Intent we compare with: " + WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-
         if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
-            Log.d("Wifireceiver", "Inside if");
             wifiIntent = new Intent(context, WifiService.class);
             wifiIntent.setAction(Constants.ACTION_GET_MAC);
             context.startService(wifiIntent);
