0b597f41df5146554fc3005ba62923bc773883d0
Maxim Goretskyy <13maxim13@live.se>
2015-10-16 11:28:45 +0200

    Refactors to check facebooktoken

--- app/src/main/java/dat255/chalmers/stormystreet/utilities/FacebookAPIProxy.java
+++ app/src/main/java/dat255/chalmers/stormystreet/utilities/FacebookAPIProxy.java
@@ -28,53 +28,54 @@ public class FacebookAPIProxy {
 
     }
     private static final long APP_ID = 733566680083257L;
-
+    /*
+        Get scores of your friends for this application.
+        Creates a list of FacebookFriends and sets to model upon success.
+     */
     public static void getScores(MainModel input){
         final List<FacebookFriend> result = new ArrayList<>();
         final MainModel model = input;
+        GraphRequest request = null;
         AccessToken facebookToken = AccessToken.getCurrentAccessToken();
-        if (facebookToken == null || facebookToken.isExpired()) {
-            Log.d("FacebookAPIProxy", "shit facebooktoken is null");
-        }
-        GraphRequest request = GraphRequest.newGraphPathRequest(
-                facebookToken,
-                "/"+APP_ID+"/scores",
-                new GraphRequest.Callback() {
-                    @Override
-                    public void onCompleted(GraphResponse response) {
-                        // Insert your code here
-                        if(response == null){
-                            Log.d("FacebookAPIProxy", "no response lol");
-                        }
-                        JSONObject facebookJson = response.getJSONObject();
-                        if(facebookJson !=null){
-                            try {
-                                JSONArray dataArr = facebookJson.getJSONArray("data");
-                                for (int i = 0; i <dataArr.length(); i++) {
-                                    String tempScore = dataArr.getJSONObject(i).getString("score");
-                                    String user = dataArr.getJSONObject(i).getString("user");
-                                    int score = Integer.parseInt(tempScore);
-                                    JSONObject tempObj = new JSONObject(user);
-                                    String userId = tempObj.getString("id");
-                                    String name = tempObj.getString("name");
-                                    result.add(new FacebookFriend(name, userId, score));
-                                    Log.d("facebook", "result in proxy " + result.toString());
-                                    //Log.d("FacebookAPIProxy", "score is " + score + " name is " + name );
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+        if (facebookToken != null && !facebookToken.isExpired()) {
+            request = GraphRequest.newGraphPathRequest(
+                    facebookToken,
+                    "/"+APP_ID+"/scores",
+                    new GraphRequest.Callback() {
+                        @Override
+                        public void onCompleted(GraphResponse response) {
+                            // Insert your code here
+                            if(response == null){
+                                Log.d("FacebookAPIProxy", "no response lol");
                             }
+                            JSONObject facebookJson = response.getJSONObject();
+                            if(facebookJson !=null){
+                                try {
+                                    JSONArray dataArr = facebookJson.getJSONArray("data");
+                                    for (int i = 0; i <dataArr.length(); i++) {
+                                        String tempScore = dataArr.getJSONObject(i).getString("score");
+                                        String user = dataArr.getJSONObject(i).getString("user");
+                                        int score = Integer.parseInt(tempScore);
+                                        JSONObject tempObj = new JSONObject(user);
+                                        String userId = tempObj.getString("id");
+                                        String name = tempObj.getString("name");
+                                        result.add(new FacebookFriend(name, userId, score));
+                                        Log.d("facebook", "result in proxy " + result.toString());
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            model.setHighscoreList(result);
                         }
-                        Log.d("FacebookAPIProxy", "Completed shit");
-                        //klar here
-                        model.setHighscoreList(result);
-                    }
-                });
+                    });
+        }
         Bundle parameters = new Bundle();
         parameters.putString("fields", "score");
-        request.setParameters(parameters);
-        request.executeAsync();
+        if(request != null){
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
 
-        Log.d("facebook", "i got here yolo");
     }
 }
