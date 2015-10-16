b40b3901a7cbce6d51ce9a7bbb753aeccaa09c51
davve94 <fodavid@student.chalmers.se>
2015-10-16 15:33:11 +0200

    added comments to DataValue

--- app/src/main/java/dat255/chalmers/stormystreet/model/DataValue.java
+++ app/src/main/java/dat255/chalmers/stormystreet/model/DataValue.java
@@ -7,44 +7,78 @@ import java.util.List;
 
 /**
  * Created by David Fogelberg on 2015-09-25.
- * Data class
+ * @author David Fogelberg
+ *
+ * This model class is used for the database SQLite and is initiated in the GlobalState class.
+ * It contains data which will be saved in the database and showed in the user interface.
+ * Data such as ID of specific data, start time, end time and distance of current bus trip.
+ *
  */
 public class DataValue {
 
     private int id;
     private List<String> dataValues;
 
+    /**
+     * A new list of dataValues which will contain bus data.
+     */
     public DataValue(){
         dataValues = new ArrayList<>();
     }
-
+    /**
+     * @return The id of a specific row.
+     */
     public long getId() {
-        // returns the id of specific row
+
         return id;
     }
+    /**
+     *
+     * @param id The new id for the bus data.
+     * Method sets a specific value id for the data when saved in the database.
+     */
     public void setId(int id) {
 
         this.id = id;
     }
-
+    /**
+     *
+     * @param values Names of data values.
+     * Method adds every data value to the list "dataValues".
+     */
     public void addValues(String... values){
         for(String value : values){
             dataValues.add(value);
         }
     }
-
+    /**
+     *
+     * @param value Name of new value.
+     * Method adds the new value to the list "dataValues".
+     *
+     */
     public void addValue(String value){
         dataValues.add(value);
     }
-
+    /**
+     *
+     * @return The list of data values.
+     */
     public List<String> getValues(){
         return this.dataValues;
     }
-
+    /**
+     *
+     * @return The amount of datavalues saved in the database.
+     */
     public int getNumberOfValues(){
         return dataValues.size();
     }
-
+    /**
+     *
+     * @return The data converted to "String" type.
+     * Method converts every data value to a "String" type.
+     */
     @Override
     public String toString() {
         String toString = "";
