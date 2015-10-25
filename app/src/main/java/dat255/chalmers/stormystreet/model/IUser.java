7f87e5904b4a6197e9bf2bc6470071a0ff16f109
Criticalstone <kevinhoog@gmail.com>
2015-10-25 11:13:29 +0100

    Removes name since it is unneccessary

--- app/src/main/java/dat255/chalmers/stormystreet/model/IUser.java
+++ app/src/main/java/dat255/chalmers/stormystreet/model/IUser.java
@@ -8,11 +8,9 @@ import java.util.List;
  * Interface for a users profile
  */
 public interface IUser {
-    String getName();
     IStatistics getStatistics();
     boolean getIsOnBus();
     void setIsOnBus(boolean isOnBus);
     int getCurrentBusNumber();
     void setCurrentBusNumber(int currentBusNumber);
-    void setName(String name);
 }
--- app/src/main/java/dat255/chalmers/stormystreet/model/User.java
+++ app/src/main/java/dat255/chalmers/stormystreet/model/User.java
@@ -5,31 +5,25 @@ package dat255.chalmers.stormystreet.model;
  * @since 2015-09-29
  */
 public class User implements IUser {
-    private String name;
     private IStatistics statistics;
     private boolean isOnBus = false;
     private int currentBusNumber;
 
-    public User(String name, IStatistics statistics, int currentBusNumber) {
-        this.name = name;
+    public User(IStatistics statistics, int currentBusNumber) {
         this.statistics = statistics;
         this.currentBusNumber = currentBusNumber;
     }
 
-    public User(String name, IStatistics statistics) {
-        this(name, statistics, 0);
+    public User(IStatistics statistics) {
+        this(statistics, 0);
     }
 
-    public User(String name, int currentBusNumber) {
-        this(name, new Statistics(), currentBusNumber);
+    public User(int currentBusNumber) {
+        this(new Statistics(), currentBusNumber);
     }
 
     public User(String name){
-        this(name, new Statistics(), 0);
-    }
-
-    public String getName() {
-        return this.name;
+        this(new Statistics(), 0);
     }
 
     public IStatistics getStatistics() {
@@ -50,9 +44,5 @@ public class User implements IUser {
 
     public void setCurrentBusNumber(int currentBusNumber) {
         this.currentBusNumber = currentBusNumber;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
